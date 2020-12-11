a5255c1d6cb0b63ac25a5bded82e04d4b1d49f3d
Kim Sang Chul <tsb03672@naver.com>
2020-12-11 15:08:16 +0900

    [Baristar] Baristar Model Added

--- baristar/models.py
+++ baristar/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.http import Http404
+from coffee_machine.models import CoffeeMachine
 
-# Create your models here.
+
+class Baristar(models.Model):
+    """
+    Baristar Model
+    """
+
+    name = models.CharField(max_length=100)
+
+    def get_baristar(self, pk):
+        try:
+            return Baristar.objects.get(pk=pk)
+        except Baristar.DoesNotExist:
+            raise Http404
+
+    def make_coffee(self, menu):
+        """
+        메뉴를 입력받아 커피 머신에서 커피를 만든다.
+        커피를 만들 수 없는 경우 False를 리턴한다
+        커피가 만들어진 경우 True를 리턴한다.
+        """
+        coffeeMachine = CoffeeMachine.objects.get(pk=1)
+        if coffeeMachine.get_coffee(menu):
+            return True
+        else:
+            return False
