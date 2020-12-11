fdda517d6772e34ee1aacfd7706fe15c6a9a03ac
Kim Sang Chul <tsb03672@naver.com>
2020-12-11 10:27:18 +0900

    [Models] Add Models (Baristar, Coffee, Coffee_Machine, Custromer, Fridge)

---
+++ baristar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BaristarConfig(AppConfig):
+    name = 'baristar'
---
+++ coffee_machine/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoffeeMachineConfig(AppConfig):
+    name = 'coffee_machine'
