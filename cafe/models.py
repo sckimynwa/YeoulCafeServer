cf7089b74e60c9d816f9a994f312dc0309d6e67a
Kim Sang Chul <tsb03672@naver.com>
2020-12-11 16:17:30 +0900

    [Cafe] Model added

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.http import Http404
+from baristar.models import Baristar
+from coffee.models import Coffee
+from coffee_machine.models import CoffeeMachine
+from fridge.models import Fridge
 
-# Create your models here.
+
+class Cafe(models.Model):
+    """
+    Cafe Model
+    """
+    balance = models.IntegerField(default=1000000)
+
+    def get_cafe(self, pk):
+        try:
+            return Cafe.objects.get(pk=pk)
+        except Cafe.DoesNotExist:
+            raise Http404
+
+    def get_coffee(self, menu):
+        coffee = Coffee.objects.get(name=menu)
+        baristar = Baristar.objects.get(pk=1)
+
+        if baristar.make_coffee(menu):
+            self.balance = self.balance + coffee.get_price()
+            self.save()
+            return True
+        else:
+            return False
